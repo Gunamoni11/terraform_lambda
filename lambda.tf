82ad2f8a8c034aa1568b40f03a307bf23d189c7f
Naresh Kumar G <93704634+Gunamoni11@users.noreply.github.com>
2022-01-12 21:49:02 +0530

    Update lambda.tf

--- lambda.tf
+++ lambda.tf
@@ -2,6 +2,6 @@ provider "aws" {
   region = "us-east-2"
 }
 resource "aws_connect_lambda_function_association" "example" {
-  function_arn = aws_lambda_function.example.arn
+  function_arn = arn:aws:lambda:us-east-2:335479894667:function:lambda_function
   instance_id  = "i-0e1c0f2ceb5a5451b"
 }
