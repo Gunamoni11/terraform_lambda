f524fb6eae62c9291b028da4fb7a9422ecfd020f
Naresh Kumar G <93704634+Gunamoni11@users.noreply.github.com>
2022-01-15 17:00:21 +0530

    Delete lambda.tf

--- lambda.tf
+++
@@ -1,7 +0,0 @@
-provider "aws" {
-  region = "us-east-2"
-}
-resource "aws_connect_lambda_function_association" "example" {
-  function_arn = "arn:aws:lambda:us-east-2:335479894667:function:lambda_function"
-  instance_id  = "i-0e1c0f2ceb5a5451b"
-}
